93440466a44a782b1a4e0751e1c8ecec9f839021
unknown <mvpolina@mail.ru>
2021-07-26 18:12:44 +0400

    Дз1,2 по автоматизации тестирования API

--- tests/my_test_DZ1.ts
+++ tests/my_test_DZ1.ts
@@ -9,17 +9,18 @@ describe('Домашнее задание 1. Автоматизация API', as
     it('Поиск случайного кота, удаление и проверка', async () => {
         //Arrange
         const params= await allure.step(
-            `выполнен запрос GET /getAllCats `,
+            `Выбирается случайный кот с помощью запроса GET /getAllCats, сохраняется его id, name`,
             async () => {
                  const response_all_cats = await CoreApi.getAllCats();
-                 const randomValue = getRandomInt(10);
-                 console.log('Выбираем кота из ', randomValue, ' алфавитной группы');
+                 const randomValue = getRandomInt(response_all_cats.data.groups.length);
+                 console.log('Выбираем кота из ', randomValue+1, ' алфавитной группы. Всего групп ', response_all_cats.data.groups.length);
                  const id = response_all_cats.data.groups[randomValue].cats[0].id;
                  const name = response_all_cats.data.groups[randomValue].cats[0].name
                  console.log('Выбран кот с id=', id, 'и именем', name);
                 const data = JSON.stringify(response_all_cats.data, null, 2);
                 allure.attachment('attachment', data, 'application/json');
                 const params=[id, name]
+                //allure.attachment('attachment2', id.toString(), 'string/Buffer');
                 return params;
             }
         );
@@ -34,7 +35,7 @@ describe('Домашнее задание 1. Автоматизация API', as
             });
         //Assert
         const status: number = 404;
-        const response_after_del = await CoreApi.searchCatByPartName(params[1]);
+        const response_after_del = await CoreApi.removeCat(params[0]);
         await allure.step(
             'Проверяется, действительно ли кот удален',
             () => {
---
+++ tests/my_test_DZ2.ts
@@ -0,0 +1,117 @@
+import { assert } from 'chai';
+import CoreApi from '../src/http/CoreApi';
+import LikeApi from '../src/http/LikeApi';
+import { allure } from 'allure-mocha/runtime';
+import Steps from '../src/steps/Steps';
+
+const getRandomInt = (max: number) => Math.floor(Math.random() * max) + 1;
+
+describe('Домашнее задание 2. Автоматизация API', async () => {
+    it('Поиск случайного кота, добавление ему n лайков и проверка', async () => {
+        //Arrange
+        const n = 5;
+        const params= await allure.step(
+            `Выбирается случайный кот с помощью запроса GET /getAllCats, сохраняется его id и количество лайков`,
+            async () => {
+                const response_all_cats = await CoreApi.getAllCats();
+                const randomValue = getRandomInt(response_all_cats.data.groups.length);
+                console.log('Выбираем кота из ', randomValue+1, ' алфавитной группы. Всего групп ', response_all_cats.data.groups.length);
+                const id = response_all_cats.data.groups[randomValue].cats[0].id;
+                const name = response_all_cats.data.groups[randomValue].cats[0].name;
+                const likes = response_all_cats.data.groups[randomValue].cats[0].likes;
+                console.log('Выбран кот с id=', id, ',именем', name, ',количеством лайков', likes);
+                const data = JSON.stringify(response_all_cats.data, null, 2);
+                allure.attachment('attachment', data, 'application/json');
+                const params=[id, likes]
+                return params;
+            }
+        );
+
+        //Act
+        const act = await allure.step(
+            'В цикле добавляются лайки',
+            async () => {
+
+                console.time('for');
+                for (let i = 0; i < n; i++) {
+                    await LikeApi.likes(params[0], {
+                        like: true,
+                        dislike: false
+                    });
+
+                }
+                console.log('Добавляется', n, ' лайков в цикле');
+                console.timeEnd('for');
+
+                return 1;
+            });
+
+        //Assert
+        const expect_likes: number = params[1]+n;
+        console.log('После добавления ожидаемое количество лайков', expect_likes);
+        const response_after = await CoreApi.getCatById(params[0]);
+        await allure.step(
+            'Проверяется, действительно ли количество лайков равно ожидаемому',
+            () => {
+                assert.ok(
+                    response_after.data.cat.likes === expect_likes,
+                    `Количество лайков не совпадает с ожидаемым`
+                );
+                console.log('Количество лайков действительно совпадает с ожидаемым');
+            });
+    });
+    it('Поиск случайного кота, добавление ему m дизлайков и проверка', async () => {
+        //Arrange
+        const m = 5;
+        const params= await allure.step(
+            `Выбирается случайный кот с помощью запроса GET /getAllCats, сохраняется его id и количество дизлайков`,
+            async () => {
+                const response_all_cats_dis = await CoreApi.getAllCats();
+                const randomValue = getRandomInt(response_all_cats_dis.data.groups.length);
+                console.log('Выбираем кота из ', randomValue+1, ' алфавитной группы. Всего групп ', response_all_cats_dis.data.groups.length);
+                const id = response_all_cats_dis.data.groups[randomValue].cats[0].id;
+                const name = response_all_cats_dis.data.groups[randomValue].cats[0].name;
+                const dislikes = response_all_cats_dis.data.groups[randomValue].cats[0].dislikes;
+                console.log('Выбран кот с id=', id, ',именем', name, ',количеством дизлайков', dislikes);
+                const data = JSON.stringify(response_all_cats_dis.data, null, 2);
+                allure.attachment('attachment', data, 'application/json');
+                const params=[id, dislikes]
+                return params;
+            }
+        );
+
+        //Act
+        const act = await allure.step(
+            'В цикле добавляются дизлайки',
+            async () => {
+
+                console.time('for');
+                for (let i = 0; i < m; i++) {
+                    await LikeApi.likes(params[0], {
+                        like: false,
+                        dislike: true
+                    });
+
+                }
+                console.log('Добавляется', m, ' дизлайков в цикле');
+                console.timeEnd('for');
+
+                return 1;
+            });
+
+        //Assert
+        const expect_dislikes: number = params[1]+m;
+        console.log('После добавления ожидаемое количество дизлайков', expect_dislikes);
+        const response_after = await CoreApi.getCatById(params[0]);
+        await allure.step(
+            'Проверяется, действительно ли количество дизлайков равно ожидаемому',
+            () => {
+                assert.ok(
+                    response_after.data.cat.dislikes === expect_dislikes,
+                    `Количество дизлайков не совпадает с ожидаемым`
+                );
+                console.log('Количество дизлайков действительно совпадает с ожидаемым');
+            });
+    });
+});
+
